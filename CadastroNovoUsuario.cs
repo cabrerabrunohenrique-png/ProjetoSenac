8fbdb203324ecb4e4acbf9cd9dc53890ddbdb29c
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-05-07 22:29:08 -0300

    Cadastro inicial com o banco de dados. Validacao de comando

--- CadastroNovoUsuario.cs
+++ CadastroNovoUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,10 +26,40 @@ namespace ProjetoSenac
 
         private void btCadastrar_Click(object sender, EventArgs e)
         {
+            string nomeCompleto = txNomeCompleto.Text;
+            int controleLinhasAftadas = 0;
+
             string dadosCadastro =
-                "server=localhost; user=root; password=; database=db_senac;";
-            using (MysqlConnection conn = new MysqlConnection(dadosCadastro))
+                "server=localhost; user=root; password=; database=bd_projetosenac;";
+
+            using (MySqlConnection conn = new MySqlConnection(dadosCadastro))
             {
+                conn.Open();
+                // tem que colocar esse comando para conecatar com o banco criado
+                //value @---serve para passar o valor do campo do formulario para o banco de dados
+                string scriptInsert = "INSERT INTO tb_cadastronovousuario (nomeCompleto) VALUE(@nomeCompleto)";
+
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsert, conn))
+                {
+                    comando.Parameters.AddWithValue("@nomeCompleto", nomeCompleto);
+
+                    controleLinhasAftadas = comando.ExecuteNonQuery();
+                        
+                }
+
+                conn.Close();
+
+            }
+
+            if (controleLinhasAftadas > 0)
+            {
+                MessageBox.Show("Cadastro realizado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Erro ao realizar cadastro.");
+
 
             }
         }
