07e13e3cb2dc527b0353e70047cb48cbc328e3aa
bhcabrera1991 <cabrerabrunohenrique@gmail.com>
2026-05-19 22:26:57 -0300

    Criando classe SAIDA ESTOQUE

--- FormEstoqueSaida.cs
+++ FormEstoqueSaida.cs
@@ -17,7 +17,7 @@ namespace ProjetoSenac
     public partial class FormEstoqueSaida : Form
     {
 
-        BindingList <SaidaEstoqueSaida> listasaida = new BindingList<SaidaEstoqueSaida>();
+        BindingList<SaidaEstoqueSaida> listasaida = new BindingList<SaidaEstoqueSaida>();
 
         public FormEstoqueSaida()
         {
@@ -42,33 +42,49 @@ namespace ProjetoSenac
             string situacaoPeca = txSituacaoPeca.Text;
 
 
+            if (dataSaida == null || codigoPeca == 0 || nomePeca == null
+                || quantidadePeca == 0 || numeroNf == 0 || cpfNf == 0
+                || numeroOs == 0 || situacaoPeca == null) ;
+
+
+
             string DADOS_CONEXAO = "server=localhost; user=root; password=; database=bdprojetosenac;";
 
             using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
             {
 
                 conn.Open();
-                string scriptInsert = "INSERT INTO tbentradaestoque" +
+                string scriptInsert = "INSERT INTO tbsaidaestoque" +
                     " (dataSaisaPeca,codigoPeca,nomePeca,quantidadePeca,nfPeca,cpfPeca,mumeroOS,situacaoPeca)" +
                     " VALUES(@dataSaisaPeca,@codigoPeca,@nomePeca,@quantidadePeca,@nfPeca,@cpfPeca,@mumeroOS,@situacaoPeca)";
 
                 using (MySqlCommand comando = new MySqlCommand(scriptInsert, conn))
                 {
-                    comando.Parameters.AddWithValue("@dataEntradaPeca", dataEntradaPeca);
-                   
-
-
-
-
+                    comando.Parameters.AddWithValue("@dataSaisaPeca", dataSaida);
+                    comando.Parameters.AddWithValue("@codigoPeca", codigoPeca);
+                    comando.Parameters.AddWithValue("@nomePeca", nomePeca);
+                    comando.Parameters.AddWithValue("@quantidadePeca", quantidadePeca);
+                    comando.Parameters.AddWithValue("@nfPeca", numeroNf);
+                    comando.Parameters.AddWithValue("@cpfPeca", cpfNf);
+                    comando.Parameters.AddWithValue("@mumeroOS", numeroOs);
+                    comando.Parameters.AddWithValue("@situacaoPeca", situacaoPeca);
                     comando.ExecuteNonQuery();
                 }
-                SaidaEstoqueSaida saindaEstoque = new SaidaEstoqueSaida();
-                saindaEstoque.DATA_SAIDA = 
+                SaidaEstoqueSaida saidaEstoque = new SaidaEstoqueSaida();
+                saidaEstoque.DATA_SAIDA = dataSaida;
+                saidaEstoque.CODIGOPECA = codigoPeca;
+                saidaEstoque.NOMEPECA = nomePeca;
+                saidaEstoque.QUANTIDADEPECA = quantidadePeca;
+                saidaEstoque.NUMERONF = numeroNf;
+                saidaEstoque.CPFPECA = cpfNf;
+                saidaEstoque.NUMEROOS = numeroOs;
+                saidaEstoque.SITUACAO = situacaoPeca;
 
 
-                listasaida.Add(saindaEstoque);
+                listasaida.Add(saidaEstoque);
 
                 conn.Clone();
             }
+        }
     }
 }
