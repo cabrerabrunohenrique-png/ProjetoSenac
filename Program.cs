ed6f66b4644675031d6310eb224a4ebd4c81e875
bhcabrera1991 <cabrerabrunohenrique@gmail.com>
2026-06-22 14:33:11 -0300

    Modificado o layout da pagina FOrmulario OS

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ProjetoSenac
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormEstoqueSaida());
+            Application.Run(new Form1());
         }
     }
 }
